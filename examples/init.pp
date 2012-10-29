1a4a6c6e5fd41006b09cc01706214a3c3e56d052
fdammeke <fabian.dammekens@cegeka.be>
2012-10-29 16:26:19 +0100

    Site and location example definition

--- examples/init.pp
+++ examples/init.pp
@@ -2,7 +2,18 @@ class { 'nginx':
   nginx_user => 'apache',
 }
 
+## Example site with location options
 nginx::site { 'test.example.com':
   ensure  => present,
-  content => '',
+  port    => '80',
+  #  root    => '/var/www/test.example.com',
+}
+
+nginx::site::location { 'testlocation':
+  site             => 'test.example.com',
+  location         => '/testlocation',
+  location_options => {
+    'rewrite'      => '^ http://www.google.be permanent',
+    'expires'      => 'max',
+  }
 }
