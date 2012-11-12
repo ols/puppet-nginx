06ef3fef69271927393acbfec17ad61d063f6658
fdammeke <fabian.dammekens@cegeka.be>
2012-11-12 15:40:36 +0100

    Enabled fastcgi config

---
+++ manifests/site/fastcgi.pp
@@ -0,0 +1,8 @@
+define nginx::site::fastcgi ($ensure=present,$options=undef) {
+
+  file { "${nginx::params::nginx_sites_enabled}/${name}.d/fastcgi.inc":
+    ensure  => $ensure,
+    content => template('nginx/includes/fastcgi.erb'),
+  }
+
+}
