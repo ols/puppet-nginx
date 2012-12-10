85ca7026817e42a6cf01f48294479a5cdb942526
luminous <luminaries@riseup.net>
2012-12-10 17:08:46 +0000

    refactor litany of file{} resources into a cleaner list, update indentation for clarity

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,68 +23,65 @@ class nginx(
   $nginx_user = 'www-data', 
   $nginx_group = 'root', 
   $nginx_worker_processes = '1', 
-  $nginx_worker_connections = '1024') 
-{
-  $nginx_includes = '/etc/nginx/includes'
-  $nginx_conf = '/etc/nginx/conf.d'
-  $nginx_home = '/var/www'
+  $nginx_worker_connections = '1024') {
+  
+    $nginx_includes = '/etc/nginx/includes'
+    $nginx_conf = '/etc/nginx/conf.d'
+    $nginx_home = '/var/www'
+  
+    if ! defined(Package['nginx']) { package { 'nginx': ensure => installed }}
+  
+    # XXX - restart-command is a quick-fix here, until
+    # http://projects.puppetlabs.com/issues/1014 is solved
+    service { 'nginx':
+      ensure     => running,
+      enable     => true,
+      hasrestart => true,
+      require    => File['/etc/nginx/nginx.conf'],
+      restart    => '/etc/init.d/nginx reload'
+    }
+  
+    file {
+      '/etc/nginx/nginx.conf':
+        ensure  => present,
+        mode    => '0644',
+        owner   => 'root',
+        group   => 'root',
+        content => template('nginx/nginx.conf.erb'),
+        notify  => Service['nginx'],
+        require => Package['nginx'];
+  
+      $nginx_conf:
+        ensure  => directory,
+        mode    => '0644',
+        owner   => 'root',
+        group   => 'root',
+        require => Package['nginx'];
+  
+      '/etc/nginx/ssl':
+        ensure  => directory,
+        mode    => '0644',
+        owner   => 'root',
+        group   => 'root',
+        require => Package['nginx'];
+  
+      $nginx_includes:
+        ensure  => directory,
+        mode    => '0644',
+        owner   => 'root',
+        group   => 'root',
+        require => Package['nginx'];
+  
+      '/etc/nginx/fastcgi_params':
+        ensure  => absent,
+        require => Package['nginx'];
+  
+      $nginx_home:
+        ensure  => directory,
+        mode    => '0755',
+        owner   => 'root',
+        group   => $nginx_group,
+        require => Package['nginx'];
+    } # end litany of file resources
 
-  if ! defined(Package['nginx']) { package { 'nginx': ensure => installed }}
-
-  #restart-command is a quick-fix here, until http://projects.puppetlabs.com/issues/1014 is solved
-  service { 'nginx':
-    ensure     => running,
-    enable     => true,
-    hasrestart => true,
-    require    => File['/etc/nginx/nginx.conf'],
-    restart    => '/etc/init.d/nginx reload'
-  }
-
-# XXX - refactor this code, condense file resources into a list, rather than separate file {}
-  file { '/etc/nginx/nginx.conf':
-    ensure  => present,
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    content => template('nginx/nginx.conf.erb'),
-    notify  => Service['nginx'],
-    require => Package['nginx'],
-  }
-
-  file { $nginx_conf:
-    ensure  => directory,
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    require => Package['nginx'],
-  }
-
-  file { '/etc/nginx/ssl':
-    ensure  => directory,
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    require => Package['nginx'],
-  }
-
-  file { $nginx_includes:
-    ensure  => directory,
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    require => Package['nginx'],
-  }
-
-  file { '/etc/nginx/fastcgi_params':
-    ensure  => absent,
-    require => Package['nginx'],
-  }
-
-  file { $nginx_home:
-    ensure  => directory,
-    mode    => '0755',
-    owner   => 'root',
-    group   => $nginx_group,
-    require => Package['nginx'],
-  } # end litany of file resources
-}
+} # end init.pp
